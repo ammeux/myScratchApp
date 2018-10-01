06a7b124e8c5cfe1db726a5e47f8ef7103043acc
ammeux <38400849+ammeux@users.noreply.github.com>
2018-10-01 22:17:34 +0200

    Add files via upload

---
+++ myScratchApp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace myScratchApp
+{
+    public partial class Form1 : Form
+    {
+        private Button playButton;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            playButton = new Button();
+
+            playButton.Location = new Point(24, 24);
+            playButton.Size = new Size(200, 24);
+            playButton.Text = "Play";
+            playButton.Click += new EventHandler(playButton_Click);
+
+            Controls.Add(playButton);
+        }
+
+        public void playButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
