cb28f394975e3a1ab5745c38c7a832813381ed06
marco75319 <marc.ammeux@ieseg.fr>
2018-10-01 22:44:29 +0200

    Add stop button

--- myScratchApp/Form1.cs
+++ myScratchApp/Form1.cs
@@ -13,20 +13,27 @@ namespace myScratchApp
     public partial class Form1 : Form
     {
         private Button playButton;
-
+        private Button stopButton;
 
 
         public Form1()
         {
             InitializeComponent();
             playButton = new Button();
+            stopButton = new Button();
 
             playButton.Location = new Point(24, 24);
             playButton.Size = new Size(200, 24);
             playButton.Text = "Play";
             playButton.Click += new EventHandler(playButton_Click);
 
+            stopButton.Location = new Point(300, 24);
+            stopButton.Size = new Size(200, 24);
+            stopButton.Text = "Stop";
+            stopButton.Click += new EventHandler(playButton_Click);
+
             Controls.Add(playButton);
+            Controls.Add(stopButton);
         }
 
         public void playButton_Click(object sender, EventArgs e)
